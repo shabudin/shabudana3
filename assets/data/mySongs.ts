9a38662f49e4280b9f7e737bde4a86fb0dd19ca7
shabudin <52714307+shabudin@users.noreply.github.com>
2019-07-24 16:07:03 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,5 @@
+import { SongInfo } from 'src/app/songInfo';
+
+export const MYSONGS: SongInfo [] = [ {songname: 'Power', artist: 'Kanye W', genre: 'Rap', year: 1999, picture: 'assets/images/Koala.jpg'},
+{songname: 'Next Song', artist: 'Unknown', genre: 'Pop', year: 2019, picture: 'assets/images/pizzfixx.jpg'}, {songname: 'Real', artist: 'Bob', genre: 'Country', year: 2018, picture: 'assets/images/pizzaaa.png'},
+{songname: 'ABC', artist: 'Oprah', genre: 'Hip-hop', year: 1995, picture: 'assets/images/pizza.jpg'}];
